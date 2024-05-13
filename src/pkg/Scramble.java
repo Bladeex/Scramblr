028a9879d7e01a5a726e2005cb10b0c9c8b2e473
Bladeex <c3041419@ncl.ac.uk>
2024-05-13 22:37:07 +0100

    Add: Scramble.java Class

---
+++ src/pkg/Scramble.java
@@ -0,0 +1,16 @@
+package pkg;
+
+// imports
+
+
+/**
+ * Scramble class that generates a scramble and stores metadata about it (time taken to complete, time created etc.)
+ * 
+ * @author Danny Bunyan-Carroll
+ * @since 13/05/2024
+ */
+public class Scramble {
+    
+    
+
+}
