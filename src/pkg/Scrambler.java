175bd4b5359a247412a0437e7fb784711ff9beea
Bladeex <c3041419@ncl.ac.uk>
2024-05-13 22:29:46 +0100

    Add: some javadoc

--- src/pkg/Scrambler.java
+++ src/pkg/Scrambler.java
@@ -3,7 +3,10 @@ package pkg;
 // imports
 
 /**
+ * Scrambler Class that provides scrambles to be inputted into a Rubik's Cube
  * 
+ * @author Danny Bunyan-Carroll
+ * @since 13/05/2024
  */
 public class Scrambler {
     
